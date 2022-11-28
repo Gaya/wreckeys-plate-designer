48a244fdd69ab28e0fed9050bf0448bc50202203
Gaya Kessler <gaya@theclevernode.com>
2022-11-28 19:55:12 +0100

    feat: show x and y in sidebar

--- src/components/SideBar/Parts/PartRow.tsx
+++ src/components/SideBar/Parts/PartRow.tsx
@@ -13,11 +13,29 @@ function PartRow({ part }: PartRowProps) {
     actions.updatePart(part.id, { name: e.target.value });
   }, [actions, part.id]);
 
+  const onUpdateOffsetX: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    actions.updatePart(part.id, { offsetX: parseFloat(e.target.value) });
+  }, [actions, part.id]);
+
+  const onUpdateOffsetY: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    actions.updatePart(part.id, { offsetY: parseFloat(e.target.value) });
+  }, [actions, part.id]);
+
   return (
     <div className="PartRow">
       <div className="PartRow_Name">
         <input type="text" value={part.name} onChange={onUpdateName} />
         <button type="button">×</button>
+      </div>
+      <div className="PartRow_Position">
+        <section>
+          <label>x:</label>
+          <input type="number" value={part.offsetX?.toFixed(2)} onChange={onUpdateOffsetX} />
+        </section>
+        <section>
+          <label>y:</label>
+          <input type="number" value={part.offsetY?.toFixed(2)} onChange={onUpdateOffsetY} />
+        </section>
       </div>
     </div>
   );
